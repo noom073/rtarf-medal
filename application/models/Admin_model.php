7a01b81e080ef5175d5f582ff0e3ba3f1383bcb6
noom073 <noom073@hotmail.com>
2020-12-19 21:20:43 +0700

    updated get_unit model function

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1,24 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin_model extends CI_Model {
+class Admin_model extends CI_Model
+{
 
     var $oracle;
 
-    function __construct() {
-        parent ::__construct();
-		$this->oracle = $this->load->database('oracle', true);
+    function __construct()
+    {
+        parent::__construct();
+        $this->oracle = $this->load->database('oracle', true);
     }
 
 
-	public function get_unit() {
-        $this->oracle->select("NPRT_UNIT, NPRT_ACM" );
-        $this->oracle->where("NPRT_UNIT = SUBSTR(NPRT_UNIT, 1,4)|| '000000'" );
-        $this->oracle->not_like("NPRT_UNIT", 6000000000);
-        $this->oracle->order_by("NPRT_UNIT");
-        $result = $this->oracle->get('PER_NPRT_TAB');
-        // echo $this->oracle->last_query();
+    public function get_unit()
+    {
+        $sql = "SELECT NPRT_UNIT, NPRT_ACM, SUBSTR(NPRT_UNIT, 1,4), SUBSTR(NPRT_UNIT, 5,6)
+            FROM PER_NPRT_TAB 
+            WHERE SUBSTR(NPRT_UNIT, 5,6) LIKE '000000' 
+            AND NPRT_UNIT NOT LIKE '6000000000'
+            ORDER BY NPRT_UNIT";
+        $result = $this->oracle->query($sql);
 
         return $result;
-	}
+    }
 }
