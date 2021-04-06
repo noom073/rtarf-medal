2d6871898f850d4c1f44124dd531a8ab1b07cdd7
noom073 <noom073@hotmail.com>
2021-04-06 22:35:23 +0700

    edit admin riboon person property

--- application/controllers/Admin_non_ribbon.php
+++ application/controllers/Admin_non_ribbon.php
@@ -44,56 +44,91 @@ class Admin_non_ribbon extends CI_Controller
         $data['type']           = $this->input->post('type');
 
         if ($ribbon == 'ท.ช.') {
-            $decArray = array('ท.ช.');
-            $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '05')->result_array();
+            if ($data['type'] == 'employee') {
+                $data['persons'] = [];
+            } else {
+                $decArray = array('ท.ช.');
+                $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '05')->result_array();
+            }
         } else if ($ribbon == 'ท.ม.') {
-            $decArray = array('ท.ม.');
-            $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '06')->result_array();
+            if ($data['type'] == 'employee') {
+                $data['persons'] = [];
+            } else {
+                $decArray = array('ท.ม.');
+                $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '06')->result_array();
+            }
         } else if ($ribbon == 'ต.ช.') {
-            $decArray = array('ต.ช.');
-            $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '07')->result_array();
+            if ($data['type'] == 'employee') {
+                $data['persons'] = [];
+            } else {
+                $decArray = array('ต.ช.');
+                $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '07')->result_array();
+            }
         } else if ($ribbon == 'ต.ม.') {
-            $decArray = array('ต.ม.');
-            $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '08')->result_array();
+            if ($data['type'] == 'employee') {
+                $data['persons'] = [];
+            } else {
+                $decArray = array('ต.ม.');
+                $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '08')->result_array();
+            }
         } else if ($ribbon == 'จ.ช.') {
             $decArray = array('จ.ช.');
-            $data['persons'] = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '09')->result_array();
+            if ($data['type'] == 'employee') {
+                $result = $this->admin_nonribbon_model->get_employee_prop($unitID, $decArray)->result_array();
+            } else {
+                $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, '09')->result_array();
+            }
+            $data['persons'] = $this->admin_nonribbon_model->jc_person_filter($result, $data['year']);
         } else if ($ribbon == 'จ.ม.') {
             $decArray = array('จ.ม.');
-            $rankAray = array('10', '11');
-            $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->jm_person_filter($result);
+            if ($data['type'] == 'employee') {
+                $result = $this->admin_nonribbon_model->get_employee_prop($unitID, $decArray)->result_array();
+            } else {
+                $rankAray = array('10', '11');
+                $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
+            }
+            $data['persons'] = $this->admin_nonribbon_model->jm_person_filter($result, $data['year']);
         } else if ($ribbon == 'บ.ช.') {
             $decArray = array('บ.ช.');
-            $rankAray = array('11', '21');
-            $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->bc_person_filter($result);
+            if ($data['type'] == 'employee') {
+                $result = $this->admin_nonribbon_model->get_employee_prop($unitID, $decArray)->result_array();
+            } else {
+                $rankAray = array('11', '21');
+                $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
+            }
+            $data['persons'] = $this->admin_nonribbon_model->bc_person_filter($result, $data['year']);
         } else if ($ribbon == 'บ.ม.') {
             $decArray = array('บ.ม.');
-            $rankAray = array('11', '21', '22', '23', '24');
-            $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->bm_person_filter($result);
+            if ($data['type'] == 'employee') {
+                $result = $this->admin_nonribbon_model->get_employee_prop($unitID, $decArray)->result_array();
+            } else {
+                $rankAray = array('11', '21', '22', '23', '24');
+                $result = $this->admin_nonribbon_model->get_person_prop($unitID, $decArray, $rankAray)->result_array();
+            }
+            $data['persons'] = $this->admin_nonribbon_model->bm_person_filter($result, $data['year']);
         } else if ($ribbon == 'ร.ท.ช.') {
             // $decArray = array('ร.ท.ช.', 'บ.ม.', 'บ.ช.');
             $rankAray = array('21', '22', '23', '24');
             $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->rtc_person_filter($result);
-        } else if ($ribbon == 'ร.ท.ม.') {
-            // $decArray = array('ร.ท.ม.');
-            $rankAray = array('25');
-            $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->rtm_person_filter($result);
-        } else if ($ribbon == 'ร.ง.ช.') {
-            // $decArray = array('ร.ง.ช.');
-            $rankAray = array('26');
-            $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->rgc_person_filter($result);
-        } else if ($ribbon == 'ร.ง.ม.') {
-            // $decArray = array('ร.ง.ม.');
-            $rankAray = array('27');
-            $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
-            $data['persons'] = $this->admin_nonribbon_model->rgm_person_filter($result);
-        } else {
+            $data['persons'] = $this->admin_nonribbon_model->rtc_person_filter($result, $data['year']);
+        }
+        // else if ($ribbon == 'ร.ท.ม.') {
+        //     // $decArray = array('ร.ท.ม.');
+        //     $rankAray = array('25');
+        //     $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
+        //     $data['persons'] = $this->admin_nonribbon_model->rtm_person_filter($result, $data['year']);
+        // } else if ($ribbon == 'ร.ง.ช.') {
+        //     // $decArray = array('ร.ง.ช.');
+        //     $rankAray = array('26');
+        //     $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
+        //     $data['persons'] = $this->admin_nonribbon_model->rgc_person_filter($result, $data['year']);
+        // } else if ($ribbon == 'ร.ง.ม.') {
+        //     // $decArray = array('ร.ง.ม.');
+        //     $rankAray = array('27');
+        //     $result = $this->admin_nonribbon_model->get_person_coin_prop($unitID, $rankAray)->result_array();
+        //     $data['persons'] = $this->admin_nonribbon_model->rgm_person_filter($result, $data['year']);
+        // } 
+        else {
             $data['persons'] = [];
         }
         // var_dump($data);
@@ -204,63 +239,111 @@ class Admin_non_ribbon extends CI_Controller
         $rsRgm = $this->admin_nonribbon_model->get_person_coin_prop($unitID, array('27'))->result_array();
         $dtrgm = $this->admin_nonribbon_model->rgm_person_filter($rsRgm);
 
-        $persons_thc_men        = array_filter($dtthc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_thc_women      = array_filter($dtthc, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_thc_men        = array_filter($dtthc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_thc_women      = array_filter($dtthc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['thc']['men']     = count($persons_thc_men);
         $data['thc']['women']   = count($persons_thc_women);
 
-        $persons_thm_men        = array_filter($dtthm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_thm_women      = array_filter($dtthm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_thm_men        = array_filter($dtthm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_thm_women      = array_filter($dtthm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['thm']['men']     = count($persons_thm_men);
         $data['thm']['women']   = count($persons_thm_women);
 
-        $persons_tc_men        = array_filter($dttc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_tc_women      = array_filter($dttc, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_tc_men        = array_filter($dttc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_tc_women      = array_filter($dttc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['tc']['men']     = count($persons_tc_men);
         $data['tc']['women']   = count($persons_tc_women);
 
-        $persons_tm_men        = array_filter($dttm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_tm_women      = array_filter($dttm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_tm_men        = array_filter($dttm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_tm_women      = array_filter($dttm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['tm']['men']     = count($persons_tm_men);
         $data['tm']['women']   = count($persons_tm_women);
-        
-        $persons_jc_men        = array_filter($dtjc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_jc_women      = array_filter($dtjc, function($r) { return $r['BIOG_SEX'] == 1; });
+
+        $persons_jc_men        = array_filter($dtjc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_jc_women      = array_filter($dtjc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['jc']['men']     = count($persons_jc_men);
         $data['jc']['women']   = count($persons_jc_women);
 
-        $persons_jm_men        = array_filter($dtjm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_jm_women      = array_filter($dtjm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_jm_men        = array_filter($dtjm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_jm_women      = array_filter($dtjm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['jm']['men']     = count($persons_jm_men);
         $data['jm']['women']   = count($persons_jm_women);
 
-        $persons_bc_men        = array_filter($dtbc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_bc_women      = array_filter($dtbc, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_bc_men        = array_filter($dtbc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_bc_women      = array_filter($dtbc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['bc']['men']     = count($persons_bc_men);
         $data['bc']['women']   = count($persons_bc_women);
 
-        $persons_bm_men        = array_filter($dtbm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_bm_women      = array_filter($dtbm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_bm_men        = array_filter($dtbm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_bm_women      = array_filter($dtbm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['bm']['men']     = count($persons_bm_men);
         $data['bm']['women']   = count($persons_bm_women);
 
-        $persons_rtc_men        = array_filter($dtrtc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_rtc_women      = array_filter($dtrtc, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_rtc_men        = array_filter($dtrtc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_rtc_women      = array_filter($dtrtc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['rtc']['men']     = count($persons_rtc_men);
         $data['rtc']['women']   = count($persons_rtc_women);
 
-        $persons_rtm_men        = array_filter($dtrtm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_rtm_women      = array_filter($dtrtm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_rtm_men        = array_filter($dtrtm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_rtm_women      = array_filter($dtrtm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['rtm']['men']     = count($persons_rtm_men);
         $data['rtm']['women']   = count($persons_rtm_women);
 
-        $persons_rgc_men        = array_filter($dtrgc, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_rgc_women      = array_filter($dtrgc, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_rgc_men        = array_filter($dtrgc, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_rgc_women      = array_filter($dtrgc, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['rgc']['men']     = count($persons_rgc_men);
         $data['rgc']['women']   = count($persons_rgc_women);
 
-        $persons_rgm_men        = array_filter($dtrgm, function($r) { return $r['BIOG_SEX'] == 0; });
-        $persons_rgm_women      = array_filter($dtrgm, function($r) { return $r['BIOG_SEX'] == 1; });
+        $persons_rgm_men        = array_filter($dtrgm, function ($r) {
+            return $r['BIOG_SEX'] == 0;
+        });
+        $persons_rgm_women      = array_filter($dtrgm, function ($r) {
+            return $r['BIOG_SEX'] == 1;
+        });
         $data['rgm']['men']     = count($persons_rgm_men);
         $data['rgm']['women']   = count($persons_rgm_women);
 
--- application/models/Admin_nonribbon_model.php
+++ application/models/Admin_nonribbon_model.php
@@ -41,6 +41,35 @@ class Admin_nonribbon_model extends CI_Model
         return $result;
     }
 
+    public function get_employee_prop($unit, $decArray)
+    {
+        $decNotIn = array_merge(array('ท.ช.', 'ป.ม.', 'ป.ช.', 'ม.ว.ม.', 'ม.ป.ช.'), $decArray);
+        $sql = "SELECT A.BIOG_IDP, A.BIOG_NAME, A.BIOG_DMY_WORK, A.BIOG_DMY_RANK, A.BIOG_SALARY, A.BIOG_POSNAME_FULL, A.BIOG_RANK,
+            A.BIOG_DEC, A.BIOG_DECY, A.BIOG_SEX, A.BIOG_SLEVEL, A.BIOG_SCLASS,
+            B.CRAK_NAME_FULL_PRINT
+            FROM PER_BIOG_VIEW A
+            INNER JOIN PER_CRAK_TAB B 
+                ON A.BIOG_RANK = B.CRAK_CODE 
+                AND A.BIOG_CDEP = B.CRAK_CDEP_CODE 
+            WHERE SUBSTR(A.BIOG_UNIT, 1,4) LIKE ?
+            AND A.BIOG_RANK IN ('50', '51')
+            AND A.BIOG_DEC NOT IN ?
+            AND substr( A.BIOG_CPOS,1,5)  in ('80169','80170','80171','80172','80179','80178','80177',
+ 	   	    '80176','80294','80293','80292','80291','80104','80103','80102','80089','80090','80091',
+ 	   	    '80092','80071','80072','80073','80079','80080','80081','80100','80099','80098','80076',
+ 	   	    '80077','80257','80256','80268','80267','80274','80275','80410','80409','80252','80251',
+ 	   	    '80062','80063','80064','80122','80124','80050','80417','80418','80419','80420','80112',
+ 	   	    '80111','80110','80109','80394','80393','80392','80391','80384','80382','80387','80309',
+ 	   	    '80310','80312','80313','80424','80421','80507','80472','80471','80470','80469','80217',
+ 	   	    '80218','80219','80220','80247','80191','80191','80192','80193','80321','80320','80137',
+ 	   	    '80139','80085','80084','80083','80082','80148')
+            ORDER BY A.BIOG_SEX, A.BIOG_RANK, A.BIOG_CDEP";
+
+        $unitID4Esc = substr($unit, 0, 4);
+        $result = $this->oracle->query($sql, array($unitID4Esc, $decNotIn));
+        return $result;
+    }
+
     public function get_person_coin_prop($unit, $rankArray)
     {
 
@@ -63,118 +92,164 @@ class Admin_nonribbon_model extends CI_Model
         return $result;
     }
 
-    public function jm_person_filter($personsArray)
+    public function jc_person_filter($personsArray, $year)
     {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = (date('Y') + 543) - substr($r['BIOG_DMY_WORK'], 4, 4);
-            $countDecYear   = (date('Y') + 543) - $r['BIOG_DECY'];
-            if (
-                ($r['BIOG_RANK'] == '10' && $countWorkYear >= 5) ||
-                (($r['BIOG_RANK'] == '11' || $r['BIOG_RANK'] == '21') && ($r['BIOG_DEC'] == 'บ.ช.' && $countDecYear >= 5))
-            ) {
-                array_push($persons, $r);
+        $persons = array_filter($personsArray, function ($r) use ($year) {
+            $decPeriod  = $year - $r['BIOG_DECY'];
+            $workPeriod = $year - $r['BIOG_DECY'];
+            $dWorkStart = substr($r['BIOG_DMY_WORK'], 0, 2);
+            $mWorkStart = substr($r['BIOG_DMY_WORK'], 2, 2);
+            if ($r['BIOG_RANK'] == '09') {
+                $flag = ($r['BIOG_DEC'] != 'จ.ช.' and $workPeriod >= 5) ? true : false;
+            } elseif (in_array($r['BIOG_RANK'], array('50', '51'))) {
+                $flag = ($r['BIOG_SALARY'] >= '12530' and $r['BIOG_DEC'] == 'จ.ม.'
+                    and $decPeriod >= 5 and ($dWorkStart < '28' and $mWorkStart <= '05')) ? true : false;
+            } else {
+                $flag = false;
             }
-        }
-
-        return $persons;
+            return $flag;
+        });
+        return array_merge($persons);
     }
 
-    public function bc_person_filter($personsArray)
+    public function jm_person_filter($personsArray, $year)
     {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countDecYear   = (date('Y') + 543) - $r['BIOG_DECY'];
-            if (
-                ($r['BIOG_RANK'] == '11' || $r['BIOG_RANK'] == '21') &&
-                ($r['BIOG_DEC'] == 'บ.ม.' && $countDecYear >= 5)
-            ) {
-                array_push($persons, $r);
+        $persons = array_filter($personsArray, function ($r) use ($year) {
+            $decPeriod  = $year - $r['BIOG_DECY'];
+            $workPeriod = $year - $r['BIOG_DECY'];
+            $dWorkStart = substr($r['BIOG_DMY_WORK'], 0, 2);
+            $mWorkStart = substr($r['BIOG_DMY_WORK'], 2, 2);
+            if ($r['BIOG_RANK'] == '10') {
+                $flag = ($workPeriod >= 5) ? true : false;
+            } elseif ($r['BIOG_RANK'] == '11' || $r['BIOG_RANK'] == '21') {
+                $flag = ($r['BIOG_DEC'] == 'บ.ช.' && $decPeriod >= 5) ? true : false;
+            } elseif (in_array($r['BIOG_RANK'], array('50', '51'))) {
+                $flag = ($r['BIOG_SALARY'] >= '12530' and $r['BIOG_DEC'] == 'บ.ช.'
+                    and $decPeriod >= 5 and ($dWorkStart < '28' and $mWorkStart <= '05')) ? true : false;
+            } else {
+                $flag = false;
             }
-        }
-
-        return $persons;
+            return $flag;
+        });
+        return array_merge($persons);
     }
 
-    public function bm_person_filter($personsArray)
+    public function bc_person_filter($personsArray, $year)
     {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = (date('Y') + 543) - substr($r['BIOG_DMY_WORK'], 4, 4);
-            $countDecYear   = (date('Y') + 543) - $r['BIOG_DECY'];
-            if (
-                ($r['BIOG_RANK'] == '11' && $countWorkYear >= 5) ||
-                (in_array($r['BIOG_RANK'], array('11', '21', '22', '23', '24')) &&
-                    ($r['BIOG_DEC'] == 'ร.ท.ช.' && $countDecYear >= 5))
-            ) {
-                array_push($persons, $r);
+        $persons = array_filter($personsArray, function ($r) use ($year) {
+            $decPeriod = $year - $r['BIOG_DECY'];
+            $dWorkStart = substr($r['BIOG_DMY_WORK'], 0, 2);
+            $mWorkStart = substr($r['BIOG_DMY_WORK'], 2, 2);
+            if (($r['BIOG_RANK'] == '11' || $r['BIOG_RANK'] == '21')) {
+                $flag = ($r['BIOG_DEC'] == 'บ.ม.' && $decPeriod >= 5) ? true : false;
+            } elseif (in_array($r['BIOG_RANK'], array('50', '51'))) {
+                if ($r['BIOG_SALARY'] >= '12530') {
+                    if ($r['BIOG_DEC'] == null or $r['BIOG_DEC'] == '') {
+                        $flag = true;
+                    } elseif (
+                        $r['BIOG_DEC'] == 'บ.ม.' and $decPeriod >= 5
+                        and ($dWorkStart < '28' and $mWorkStart <= '05')
+                    ) {
+                        $flag = true;
+                    }
+                } elseif ($r['BIOG_SALARY'] >= '6800' and $r['BIOG_SALARY'] <= '12529') {
+                    $flag = ($r['BIOG_DEC'] == 'บ.ม.' and $decPeriod >= 5
+                        and ($dWorkStart < '28' and $mWorkStart <= '05')) ? true : false;
+                } else {
+                    $flag = false;
+                }
+            } else {
+                $flag = false;
             }
-        }
-
-        return $persons;
+            return $flag;
+        });
+        return array_merge($persons);
     }
 
-    public function rtc_person_filter($personsArray)
+    public function bm_person_filter($personsArray, $year)
     {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = (date('Y') + 543) - substr($r['BIOG_DMY_WORK'], 4, 4);
-            if (
-                in_array($r['BIOG_RANK'], array('21', '22', '23', '24')) &&
-                $countWorkYear >= 5
-            ) {
-                array_push($persons, $r);
+        $persons = array_filter($personsArray, function ($r) use ($year) {
+            $decPeriod = $year - $r['BIOG_DECY'];
+            $workPeriod = $year - $r['BIOG_DECY'];
+            if (in_array($r['BIOG_RANK'], array('11', '21', '22', '23', '24'))) {
+                if ($r['BIOG_RANK'] == '11') {
+                    $flag = ($workPeriod >= 5) ? true : false;
+                } else {
+                    $flag = ($r['BIOG_DEC'] == 'ร.ท.ช.' && $decPeriod >= 5) ? true : false;
+                }
+            } elseif (in_array($r['BIOG_RANK'], array('50', '51'))) {
+                $flag = ($r['BIOG_SALARY'] >= '6800' and $r['BIOG_SALARY'] <= '12529'
+                    and ($r['BIOG_DEC'] == null or $r['BIOG_DEC'] == '')) ? true : false;
+            } else {
+                $flag = false;
             }
-        }
-
-        return $persons;
+            return $flag;
+        });
+        return array_merge($persons);
     }
 
-    public function rtm_person_filter($personsArray)
+    public function rtc_person_filter($personsArray, $year)
     {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = date('Y') - substr($r['BIOG_DMY_WORK'], 4, 4);
-            if (
-                in_array($r['BIOG_RANK'], array('25')) &&
-                $countWorkYear >= 5
-            ) {
-                array_push($persons, $r);
+        $persons = array_filter($personsArray, function ($r) use ($year) {
+            $workPeriod = $year - $r['BIOG_DECY'];
+            if (in_array($r['BIOG_RANK'], array('21', '22', '23', '24')) && $workPeriod >= 5) {
+                $flag = true;
+            } else {
+                $flag = false;
             }
-        }
-
-        return $persons;
+            return $flag;
+        });
+        return array_merge($persons);
     }
 
-    public function rgc_person_filter($personsArray)
-    {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = date('Y') - substr($r['BIOG_DMY_WORK'], 4, 4);
-            if (
-                in_array($r['BIOG_RANK'], array('26')) &&
-                $countWorkYear >= 5
-            ) {
-                array_push($persons, $r);
-            }
-        }
+    // public function rtm_person_filter($personsArray, $year)
+    // {
+    //     $persons = array_filter($personsArray, function ($r) use ($year) {
+    //         $workPeriod = $year - $r['BIOG_DECY'];
+    //         if (
+    //             in_array($r['BIOG_RANK'], array('25')) &&
+    //             $workPeriod >= 5
+    //         ) {
+    //             $flag = true;
+    //         } else {
+    //             $flag = false;
+    //         }
+    //         return $flag;
+    //     });
+    //     return array_merge($persons);
+    // }
 
-        return $persons;
-    }
+    // public function rgc_person_filter($personsArray, $year)
+    // {
+    //     $persons = array_filter($personsArray, function ($r) use ($year) {
+    //         $workPeriod = $year - $r['BIOG_DECY'];
+    //         if (
+    //             in_array($r['BIOG_RANK'], array('26')) &&
+    //             $workPeriod >= 5
+    //         ) {
+    //             $flag = true;
+    //         } else {
+    //             $flag = false;
+    //         }
+    //         return $flag;
+    //     });
+    //     return array_merge($persons);
+    // }
 
-    public function rgm_person_filter($personsArray)
-    {
-        $persons = array();
-        foreach ($personsArray as $r) {
-            $countWorkYear  = date('Y') - substr($r['BIOG_DMY_WORK'], 4, 4);
-            if (
-                in_array($r['BIOG_RANK'], array('27')) &&
-                $countWorkYear >= 5
-            ) {
-                array_push($persons, $r);
-            }
-        }
-
-        return $persons;
-    }
+    // public function rgm_person_filter($personsArray, $year)
+    // {
+    //     $persons = array_filter($personsArray, function ($r) use ($year) {
+    //         $workPeriod = $year - $r['BIOG_DECY'];
+    //         if (
+    //             in_array($r['BIOG_RANK'], array('27')) &&
+    //             $workPeriod >= 5
+    //         ) {
+    //             $flag = true;
+    //         } else {
+    //             $flag = false;
+    //         }
+    //         return $flag;
+    //     });
+    //     return array_merge($persons);
+    // }
 }
